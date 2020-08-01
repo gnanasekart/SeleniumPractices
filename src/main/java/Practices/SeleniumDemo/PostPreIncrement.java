d158b7951b7034f21ad099dd7fd6d9446533959a
gnanasekart <gnanasekar69@hotmail.com>
2020-08-01 15:25:03 +0530

    initial commit

---
+++ src/main/java/Practices/SeleniumDemo/PostPreIncrement.java
@@ -0,0 +1,20 @@
+package Practices.SeleniumDemo;
+
+public class PostPreIncrement {
+
+	public static void main(String[] args)
+	{
+        int a;
+		int b ;
+		int c 
+		;for (int i=0; i<5; i++ )
+		{
+			 a = 10;
+			 b = 20;
+			 c = (++a + ++b);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+		}
+}
